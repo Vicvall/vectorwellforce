f2519e598e19816ec08e9bd647a321453fc410a8
Victor Vällfors <victor.vallfors@hotmail.com>
2020-07-21 17:38:34 +0200

    Added npm-watch, made header with buttons that don't work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logofront from './vectorwellforce_logofront.svg';
 import logo from './vectorwellforce_logotext.svg';
 import logoback from './vectorwellforce_logoback.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -9,12 +10,19 @@ function App() {
     <div className="App">
       <title>Vector Well Force</title>
       <header className="App-header">
-        <div class="logo">
-          <img src={logoback} className="App-logo2" alt="logo" />
-          <img src={logo} className="App-logo-text" alt="logo" />
-          <img src={logofront} className="App-logo" alt="logo" />
+          <div className="Logo">
+            <img src={logoback} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo-text" alt="logo" />
+            <img src={logofront} className="App-logo" alt="logo" />
+          </div>
+        <div className="Navigation">
+          <Button className="NavButton">Button 1</Button>
+          <Button className="NavButton">Button 2</Button>
         </div>
       </header>
+      <body className="body">
+        <p>Testing the font</p>
+      </body>
     </div>
   );
 }
