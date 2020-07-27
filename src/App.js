a837a7cc3ebb65475ba8d4d533ce068830baf7b3
Victor Vällfors <victor.vallfors@hotmail.com>
2020-07-27 19:08:01 +0200

    Made whole page blue

--- src/App.js
+++ src/App.js
@@ -10,18 +10,22 @@ function App() {
     <div className="App">
       <title>Vector Well Force</title>
       <header className="App-header">
+        <div className="App-header-menu">
           <div className="Logo">
             <img src={logoback} className="App-logo" alt="logo" />
             <img src={logo} className="App-logo-text" alt="logo" />
             <img src={logofront} className="App-logo" alt="logo" />
           </div>
-        <div className="Navigation">
-          <Button className="NavButton">Button 1</Button>
-          <Button className="NavButton">Button 2</Button>
+          <div className="Navigation">
+            <Button className="NavButton">Button 1</Button>
+            <Button className="NavButton">Button 2</Button>
+          </div>
         </div>
       </header>
       <body className="body">
-        <p>Testing the font</p>
+        <div className="content">
+          <p>Testing the font</p>
+        </div>
       </body>
     </div>
   );
