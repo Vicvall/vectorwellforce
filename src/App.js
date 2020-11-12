4f87885ae350194ac73db86ad121d84dd58b7801
Victor <victor.vallfors@hotmail.com>
2020-11-12 05:33:47 +0100

    Added crappy scrolling nav, needs more work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { HashRouter as BrowserRouter, Switch, Route} from "react-router-dom"; //remove hashrouter alias before deploy or bad times might happen
-import logofront from './vectorwellforce_logofront.svg';
-import logo from './vectorwellforce_logotext.svg';
-import logoback from './vectorwellforce_logoback.svg';
+import { HashRouter as BrowserRouter, Switch, Route} from "react-router-dom"; //remove hashrouter alias before deploy or bad times might happen, using hashrouter because browserrouter fails locally when used on files
 import './App.css';
 
 import Navbar from './components/Navbar.js'
@@ -14,16 +11,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <title>Vector Well Force</title>
-        <div className="App-header">
-          <div className="App-header-menu">
-            <div className="Logo">
-              <img src={logoback} className="App-logo" alt="logo" />
-              <img src={logo} className="App-logo-text" alt="logo" />
-              <img src={logofront} className="App-logo" alt="logo" />
-            </div>
-            <Navbar />
-          </div>
-        </div>
+        <Navbar />
         <div className="body">
           <Switch>
             <Route exact path="/blog">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
+import React, {useEffect} from "react";
 import { NavLink } from "react-router-dom";
 
+import logofront from '../vectorwellforce_logofront.svg';
+import logo from '../vectorwellforce_logotext.svg';
+import logoback from '../vectorwellforce_logoback.svg';
+
 const Navbar = () => {
+    const [ scrolled, setScrolled ] = React.useState(false);
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if(offset > 1){
+          setScrolled(true);
+        }
+        else{
+          setScrolled(false);
+        }
+      }
+    
+      useEffect(() => {
+        window.addEventListener('scroll',handleScroll)
+      })
+    let navbarClasses=['App-header-menu'];
+      if(scrolled){
+        navbarClasses.push('scrolled');
+      }
+
     return (
-        <div className="Navigation">
-            <NavLink className="NavButton" to="/blog">Blog</NavLink>
-            <NavLink className="NavButton" to="/about">About</NavLink>
-      </div>
+        <div className="App-header">
+            <div className={navbarClasses.join(" ")}>
+                <div className="Logo">
+                    <img src={logoback} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo-text" alt="logo" />
+                    <img src={logofront} className="App-logo" alt="logo" />
+                </div>
+                <div className="Navigation">
+                    <NavLink className="NavButton" to="/blog">Blog</NavLink>
+                    <NavLink className="NavButton" to="/about">About</NavLink>
+                </div>
+            </div>
+        </div>
     );
+
+//<div className="App-header">
+//<div className="App-header-menu">
 
 //   const [isOpen, setOpen] = useState(false);
 //   return ( 
